495d7218dd53bae03449b0465a9598c0a56e6198
YoungAncient <68391309+YoungAncient@users.noreply.github.com>
2021-02-02 23:39:29 -0500

    added code for footer component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,33 @@
+// Import React
+import React from 'react';
+
+// render Footer
+function Footer() {
+    // add array of links and accompanying icons for spaces where I can be contacted
+    const socials = [
+        {
+            name: 'fab fa-github',
+            link: 'https://github.com/SterlynKong'
+        },
+        {
+            name: 'fab -fa-linkedin',
+            link: 'www.linkedin.com/in/sterlyn-k-487144bb'
+        },
+        {
+            name: 'fab fa-email',
+            link: '<a href="mailto:sterlyn.kong@icloud.com">'
+        }
+    ]
+
+    // map socials array and place in footer
+    return (
+        <footer className='flex-row px-1'>
+            {socials.map(socialConnection => (
+                <a href={socialConnection.link} key={socialConnection.name} target='_blank' rel='noopener noreferrer'><i className={socialConnection.name}></i></a>
+            )
+            )}
+        </footer>
+    );
+};
+
+export default Footer;
