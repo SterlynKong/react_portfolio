0927a575ce41fde9b596fc9a585035dc876737b6
YoungAncient <68391309+YoungAncient@users.noreply.github.com>
2021-02-02 21:46:28 -0500

    setup Nav structure

---
+++ src/components/Nav/index.js
@@ -0,0 +1,17 @@
+// import react and useEffect method
+import React, { useEffect } from 'react';
+
+
+// render Nav
+function Nav (props) {
+    return (
+        <nav>
+            <ul className="flex-row">
+                
+            </ul>
+        </nav>
+    );
+};
+
+
+export default Nav;
