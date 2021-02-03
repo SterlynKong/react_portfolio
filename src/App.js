cd5af52aac07da8715d74d549e59657a8edc3a25
YoungAncient <68391309+YoungAncient@users.noreply.github.com>
2021-02-02 21:30:32 -0500

    added code for app structure

--- src/App.js
+++ src/App.js
@@ -1,4 +1,31 @@
+// Import React and useState method
 import React, { useState } from 'react';
 
+// Import Header component
+import Header from './components/Header';
 
+// Import Nav component
+import Nav from './components/Nav';
+
+// Import Footer component
+import Footer from './components/Footer';
+
+// App structure - jsx
+function App() {
+  return (
+    <div>
+      <Header>
+        <Nav>
+
+        </Nav>
+      </Header>
+      <main>
+
+      </main>
+      <Footer>
+
+      </Footer>
+    </div>
+  );
+};
 export default App;
